7b8d8b8aef4a06ef043ad4713349c82ed8f5f7e6
Zach <chiovaro@gmail.com>
2014-01-16 16:58:31 -0500

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,61 @@
+
+
+<!-- FOOTER-->
+<footer class="row-fluid">
+      <div class="span3">
+        <h4 class="line3 center standart-h4title"><span>Navigation</span></h4>
+        <ul class="footer-links">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <li><a href="admin/index.php">Admin</a></li>
+        </ul>
+      </div>
+    
+    <div class="span3">
+        <h4 class="line3 center standart-h4title"><span>Useful Links</span></h4>
+        <ul class="footer-links">
+          <li><a href="http://getbootstrap.com/">Bootstrap</a></li>
+          <li><a href="http://www.w3schools.com/html/html5_intro.asp">w3 Schools</a></li>
+         <li><a href="http://cs.gsu.edu/">GSU C.S</a></li>
+        </ul>
+    </div> 
+    
+  <div class="span3">
+        <h4 class="line3 center standart-h4title"><span>Useful Links</span></h4>
+        <ul class="footer-links">
+          <li><a href="http://www.sublimetext.com/">Submlime Text</a></li>
+          <li><a href="http://php.net/">PHP.net</a></li>
+          <li><a href="http://www.mysql.com/">MySQL</a></li>
+        </ul>
+    </div>
+  
+    <div class="span3">
+        <h4 class="line3 center standart-h4title"><span>Find Me</span></h4>
+    <ul class="footer-links">
+      <li>Zach Chiovaro</li>
+      <li><a href="https://www.facebook.com/chiovaro"><i class="fa fa-facebook fa-1x"></i> CHIOVARO</a></li>
+      <li><a href="https://twitter.com/zachiocom"><i class="fa fa-twitter fa-1x"></i> @ZachioCOM</a></li>
+    
+    </ul>
+    </div>
+  
+<div class="span12">
+  <hr>
+        <p class="muted credit"><small>© Zachio.com Company 2013</small></p>
+</div>
+    
+</footer>
+
+</div>
+<!-- /CONTENT SIDE-->
+   
+
+
+
+</div>
+
+ 
+
+ 
+</div> <!-- /container -->
+<!--/ CONTENT -->
