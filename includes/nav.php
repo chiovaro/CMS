56ec8ddf896bdf1cd00af0457ae9c411f1b0d595
Zach <chiovaro@gmail.com>
2014-01-16 16:59:03 -0500

    Create nav.php

---
+++ includes/nav.php
@@ -0,0 +1,54 @@
+   <div class="container">
+ <div class="navbar">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+    
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="index.php">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="contact.php">Contact</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="Zachsresume.pdf">Resume</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Users</li>
+                  <li><a href="#">Sign Up</a></li>
+                  <li><a href="#">Login</a></li>
+                </ul>
+              </li>
+                  <form class="navbar-search pull-left" action="search.php" method="get" enctype="mutlipart/form-data">
+                    <input type="text" class="search-query" placeholder="Search" name="value">
+                    <input type="submit" names="search" value="Search">
+                  </form>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+</div>
+
+    <!-- PAGE-HEADER-->
+      <div class="page-header">
+    <div class="container">
+      <!--      <div class="row">
+        <div class="span4">
+          <h1><a class="brand" href="index.php">ZACHIO</a></h1>
+        </div>
+        <div class="span8">
+          <div class="well text-center">
+            <a href="#">www.zachio.com</a> ADDS HERE
+          </div>
+        </div>
+      </div> -->    
+      
+      </div>
+<!-- PAGE-HEADER-->
+</div>
